efe8a55cf13efa784e10535f80782934a30e35f5
Piper Merriam <pipermerriam@gmail.com>
2015-10-07 13:48:59 -0600

    finish out tests

--- tests/simple-patterns/test_simple_patterns.py
+++ tests/simple-patterns/test_simple_patterns.py
@@ -1,19 +1,19 @@
 import time
 import datetime
+import pytz
 
 
 def to_timestamp(dt):
-    offset = time.mktime(datetime.datetime(1970, 1, 1).timetuple())
-    return int(time.mktime(dt.timetuple()) - offset)
+    offset = time.mktime(datetime.datetime(1970, 1, 1, tzinfo=pytz.UTC).timetuple())
+    return int(time.mktime(dt.replace(tzinfo=pytz.UTC).timetuple()) - offset)
 
 
-def test_yearly(deployed_contracts):
-    crontab = deployed_contracts.Crontab
-    dt_now = datetime.datetime.now()
-    now = to_timestamp(dt_now)
+def test_yearly(crontab):
+    dt_origin = datetime.datetime(2014, 5, 15, 1, 2, 3)
+    now = to_timestamp(dt_origin)
     next_occurrence = crontab.next("0", "0", "0", "1", "1", "*", "*", now)
     dt = datetime.datetime.utcfromtimestamp(next_occurrence)
-    assert dt.year == dt_now.year + 1
+    assert dt.year == 2015
     assert dt.month == 1
     assert dt.day == 1
     assert dt.hour == 0
@@ -21,24 +21,22 @@ def test_yearly(deployed_contracts):
     assert dt.second == 0
 
 
-def test_monthly(deployed_contracts):
-    crontab = deployed_contracts.Crontab
-    dt_now = datetime.datetime.now()
-    now = to_timestamp(dt_now)
+def test_monthly(crontab):
+    dt_origin = datetime.datetime(2014, 5, 15, 1, 2, 3)
+    now = to_timestamp(dt_origin)
     next_occurrence = crontab.next("0", "0", "0", "1", "*", "*", "*", now)
     dt = datetime.datetime.utcfromtimestamp(next_occurrence)
-    assert dt.month == dt_now.month + 1 if dt_now.month < 12 else 1
+    assert dt.year == 2014
+    assert dt.month == 6
     assert dt.day == 1
     assert dt.hour == 0
     assert dt.minute == 0
     assert dt.second == 0
 
 
-def test_weekly(deployed_contracts):
-    crontab = '0 0 0 * * 0'
-    crontab = deployed_contracts.Crontab
-    dt_now = datetime.datetime.now()
-    now = to_timestamp(dt_now)
+def test_weekly(crontab):
+    dt_origin = datetime.datetime(2014, 5, 15, 1, 2, 3)
+    now = to_timestamp(dt_origin)
     next_occurrence = crontab.next("0", "0", "0", "*", "*", "1", "*", now)
     dt = datetime.datetime.utcfromtimestamp(next_occurrence)
     assert dt.weekday() == 0  # Monday
@@ -47,11 +45,22 @@ def test_weekly(deployed_contracts):
     assert dt.second == 0
 
 
-def test_daily(deployed_contracts):
-    crontab = '0 0 0 * * *'
-    assert False
+def test_daily(crontab):
+    dt_origin = datetime.datetime(2014, 5, 15, 1, 2, 3)
+    now = to_timestamp(dt_origin)
+    next_occurrence = crontab.next("0", "0", "0", "*", "*", "*", "*", now)
+    dt = datetime.datetime.utcfromtimestamp(next_occurrence)
+    assert dt.day == 16
+    assert dt.hour == 0
+    assert dt.minute == 0
+    assert dt.second == 0
 
 
-def test_hourly(deployed_contracts):
-    crontab = '0 0 * * * *'
-    assert False
+def test_hourly(crontab):
+    dt_origin = datetime.datetime(2014, 5, 15, 1, 2, 3)
+    now = to_timestamp(dt_origin)
+    next_occurrence = crontab.next("0", "0", "*", "*", "*", "*", "*", now)
+    dt = datetime.datetime.utcfromtimestamp(next_occurrence)
+    assert dt.hour == 2
+    assert dt.minute == 0
+    assert dt.second == 0
